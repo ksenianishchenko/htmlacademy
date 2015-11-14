e294c2c892f384a1c0b282f9ee2b27666cef3987
Ksenia Anishchenko <kseninavoi@gmail.com>
2015-11-14 10:17:34 -0800

    Последние изменения

--- Sedona/js/script.js
+++ Sedona/js/script.js
@@ -1,36 +1,33 @@
+var link = document.querySelector('.form-search-hotel');
+var popup = document.querySelector('.form-search');
+var moveIn = popup.querySelector('[name=dateIn]');
+var moveOut = popup.querySelector ('[name=dateOut]');
 
-        var link = document.querySelector(".form-search-hotel");
-        var popup = document.querySelector(".form-search");
-        var moveIn = popup.querySelector("[name=dateIn]");
-        var moveOut = popup.querySelector ("[name=dateOut]");
-        
-        
-        link.addEventListener("click", function(event) {
-            event.preventDefault();
-            popup.classList.toggle("show");
-            
-            setTimeout(function() {
-                moveIn.focus();
-            }, 300);
-        });
+link.addEventListener('click', function(event) {
+    event.preventDefault();
+    popup.classList.toggle('show');
+    setTimeout(function() {
+        moveIn.focus();
+    }, 300);
+});
 
+popup.addEventListener('submit', function(event) {
+    if (!moveIn.value || !moveOut.value) {
+        event.preventDefault();
+        popup.classList.remove('show-error');
+        popup.offsetWidth = popup.offsetWidth;
+        popup.classList.add('show-error');
+        }
+    else {
+        localStorage.setItem('moveIn',moveIn.value);
+        }
+});
 
-        
-        popup.addEventListener("submit", function(event) {
-            if (!moveIn.value || !moveOut.value) 
-                {event.preventDefault();
-                popup.classList.remove("show-error");
-                popup.offsetWidth = popup.offsetWidth;
-                popup.classList.add("show-error");}
-            else {
-                localStorage.setItem("moveIn",moveIn.value);}
-        });
-        
-        window.addEventListener("keydown", function(event) {
-            if (event.keyCode === 27) {
-                if (popup.classList.contains("show")) {
-                    popup.classList.remove("show");
-                }
-            }
-        });
-        
+window.addEventListener('keydown', function(event) {
+    if (event.keyCode === 27) {
+        if (popup.classList.contains('show')) {
+            popup.classList.remove('show');
+        }
+    }
+});
+
